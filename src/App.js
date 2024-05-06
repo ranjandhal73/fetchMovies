48cd10f9a4f9e0e16ed5ee46d5e3f3780fc13f74
ranjandhal73 <ranjandhal.doc@gmail.com>
2024-05-06 20:58:12 +0530

    Making apps error proof

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
   const [movie, setMovie] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [retryTimer, setRetryTimer] = useState(null);
 
   const fetchMoviesHandler = async () =>{
     setIsLoading(true);
+    setError(null);
    try {
-    const response = await fetch ('https://swapi.dev/api/films');
+    const response = await fetch ('https://swapi.dev/api/films0');
+
     if(!response.ok){
-      throw new Error('Something went wrong!');
+      throw new Error(`Something went wrong! ...Retrying `);
     }
+  
     const data = await response.json();
     const transferedMovie = data.results.map((movie)=>{
       return {
@@ -24,10 +29,17 @@ function App() {
       }
     })
     setMovie(transferedMovie);
-   } catch (error) {
-    console.log(error);
-   }
-   setIsLoading(false)
+  } catch (error) {
+    setError(error.message)
+    setRetryTimer(setTimeout(()=>fetchMoviesHandler,500));
+  }
+  setIsLoading(false)
+  }
+
+  const cancelHandler = () =>{
+      clearTimeout(retryTimer)
+      setError(null);
+      setIsLoading(false)
   }
 
   return (
@@ -38,6 +50,13 @@ function App() {
       <section>
         {!isLoading && <MoviesList movies={movie} />}
         {isLoading && <span className="loader"></span>}
+        {!isLoading && error && (
+          <div>
+            <p>{error}</p>
+            <button onClick={cancelHandler}>Cancel</button>
+          </div>
+        )}
+        
       </section>
     </React.Fragment>
   );
